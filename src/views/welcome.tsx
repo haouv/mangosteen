05b334ec7b48a4ebb3950779a2043eafbcdb3c64
wuhao <wuhao8882@gmail.com>
2022-07-21 18:07:50 +0800

    增加 welcome 页面切换动画

--- src/views/welcome.tsx
+++ src/views/welcome.tsx
@@ -1,21 +1,34 @@
-import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
+import { defineComponent, Transition, VNode } from "vue";
+import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
 import MangoLogo from "../assets/icons/mangosteen.svg";
 import s from "./welcome.module.scss";
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    return () => <div class={s.wrapper}>
-      <header>
-        <img src={MangoLogo} alt="" />
-        <h1>山竹记账</h1>
-      </header>
-      <main class={s.main}>
-        <RouterView name="main"/>
-      </main>
-      <footer class={s.actions}>
-        <RouterView name="footer"></RouterView>
-      </footer>
-    </div>
+    return () => (
+      <div class={s.wrapper}>
+        <header>
+          <img src={MangoLogo} alt="" />
+          <h1>山竹记账</h1>
+        </header>
+        <main class={s.main}>
+          <RouterView name="main">
+            {({ Component, route}: { Component: VNode; route: RouteLocationNormalizedLoaded }) => (
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {Component}
+              </Transition>
+            )}
+          </RouterView>
+        </main>
+        <footer class={s.actions}>
+          <RouterView name="footer"></RouterView>
+        </footer>
+      </div>
+    );
   }
 });
