e9deab20278046b831175fed33e793daf390f440
wuhao <wuhao8882@gmail.com>
2022-07-22 15:36:52 +0800

    add Button component

--- src/views/startpage.tsx
+++ src/views/startpage.tsx
@@ -1,3 +1,15 @@
-export const StartPage = () => {
-  return <div>StartPage</div>
-}
+import { defineComponent } from "vue";
+import { Button } from "../shared/button";
+import s from "./startpage.module.scss";
+
+export const StartPage = defineComponent({
+  setup: () => {
+    return () => (
+      <div class={s.startpage}>
+        <div class={s.button_wrapper}>
+          <Button class={s.button}>开始记账</Button>
+        </div>
+      </div>
+    )
+  },
+});
