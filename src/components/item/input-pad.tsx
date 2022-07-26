b3713e7695c1e21d76c3d0cf5400093a4615df2e
wuhao <wuhao8882@gmail.com>
2022-07-26 17:57:18 +0800

    time helper function input-pad 输入金额验证

--- src/components/item/input-pad.tsx
+++ src/components/item/input-pad.tsx
@@ -1,11 +1,39 @@
 import { defineComponent, ref } from "vue";
 import { Icon } from "../../shared/icon";
+import { Popup, DatetimePicker } from 'vant';
+import { time } from "../../shared/time";
 import s from "./input-pad.module.scss";
+import 'vant/es/datetime-picker/style';
+import 'vant/es/popover/style';
 
 export const InputPad = defineComponent({
+
   setup: (props, context) => {
+    const now = new Date();
     const refAmount = ref<string>("");
-    const appendText = (n: string | number) => { refAmount.value += n; }
+    const refDatePickerVisible = ref<boolean>(false);
+    const refDate = ref<Date>(now);
+    const showDatePicker = () => {
+      refDatePickerVisible.value = !refDatePickerVisible.value
+    }
+    const appendText = (n: string | number) => { 
+      if (refAmount.value == '0') {
+        refAmount.value = String(n);
+        return;
+      }
+      const pattern = /^([1-9]\d{0,9}|0)(\.\d{1,2})?$/;
+      const pattern2 = /^([1-9]\d{0,9}|0)(\.)?$/;
+      const amount = refAmount.value + n; 
+      console.log(refAmount.value, n, amount);
+      if (pattern.test(amount) || pattern2.test(amount)) {
+        refAmount.value = amount;
+      }
+    }
+    const onDatetimeConfirm = (value: Date) => {
+      refDate.value = value
+      refDatePickerVisible.value = false;
+    }
+
     const buttons = [
       { text: '1', onClick: () => { appendText(1) } },
       { text: '2', onClick: () => { appendText(2) } },
@@ -25,9 +53,16 @@ export const InputPad = defineComponent({
       <div class={s.dateAndAmount}>
         <span class={s.date}>
           <Icon name="date" class={s.icon}></Icon>
-          <span>2020-01-01</span>
+          <span>
+            <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+            <Popup v-model:show={refDatePickerVisible.value} position='bottom' >
+              <DatetimePicker v-model={refDate.value} type="date" title="选择年月日"
+                onConfirm={onDatetimeConfirm} 
+                onCancel={() => { refDatePickerVisible.value = false }} />
+            </Popup>
+          </span>
         </span>
-        <span class={s.amount}>123456789</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(button => <button onClick={button.onClick}><div>{button.text}</div></button>)}
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import {svgstore} from './src/vite_plugins/svgstore';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +13,9 @@ export default defineConfig({
       transformOn: true,
       mergeProps: true,
     }),
-    svgstore()
+    svgstore(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
 });
