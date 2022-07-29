f26985e03a23c88e561d7d998263e5b6d014f6bd
wuhao <wuhao8882@gmail.com>
2022-07-29 12:52:12 +0800

    按钮组件增加level

--- src/shared/button.tsx
+++ src/shared/button.tsx
@@ -9,11 +9,18 @@ export const Button = defineComponent({
   props: {
     type: {
       type: String as PropType<'submit' | 'normal'>
+    },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
+    },
+    level: {
+      type: String as PropType<'important' | 'normal' | 'danger'>,
+      default: 'important'
     }
   },
   setup: (props, context) => {
     return () => (
-      <button class={s.button}>
+      <button class={[s.button, s[props.level]]}>
         {context.slots.default?.()}
       </button>
     )
