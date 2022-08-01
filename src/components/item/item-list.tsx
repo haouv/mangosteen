27dd845bac88bf642c82a304bb5983cc192ceb32
wuhao <wuhao8882@gmail.com>
2022-08-01 17:18:07 +0800

    增加items页面 自定义时间弹窗

--- src/components/item/item-list.tsx
+++ src/components/item/item-list.tsx
@@ -1,5 +1,7 @@
+import { Overlay } from "vant";
 import { defineComponent, PropType, reactive, ref } from "vue";
 import { MainLayout } from "../../layouts/main-layout";
+import { Form, FormItem } from "../../shared/form";
 import { Icon } from "../../shared/icon";
 import { Tab, Tabs } from "../../shared/tabs";
 import { Time } from "../../shared/time";
@@ -9,47 +11,99 @@ import { ItemSummary } from "./item-summary";
 export const ItemList = defineComponent({
   setup: () => {
     const selectTab = ref<string>("本月");
+    const refOverlayVisible = ref<boolean>(false);
     const time = new Time();
     const customTime = reactive({
-      startDate: new Time().format(),
-      endDate: new Time().format(),
-    })
+      start: new Time().format(),
+      end: new Time().format(),
+    });
     const timeList = [
       { start: time.firstDayOfMonth(), end: time.lastDayOfMonth() },
-      { start: time.add(-1, "month").firstDayOfMonth(), end: time.add(-1, "month").lastDayOfMonth() },
+      {
+        start: time.add(-1, "month").firstDayOfMonth(),
+        end: time.add(-1, "month").lastDayOfMonth(),
+      },
       { start: time.firstDayOfYear(), end: time.lastDayOfYear() },
-    ]
+    ];
+
+    const onSubmitCustomTime = (e: Event) => {};
     return () => (
       <MainLayout>
         {{
           title: () => "山竹记账",
-          icon: () => <Icon name="nav" onClick={() => { }} />,
+          icon: () => <Icon name="nav" onClick={() => {}} />,
           default: () => (
-            <Tabs class={s.items} classPrefix={'custom_items'}
-              v-model:selected={selectTab.value}
-              onUpdate:selected={() => {
-                console.log('onUpdate:selected');
-              }}>
-              <Tab name="本月" class={s.tab}>
-                <ItemSummary startDate={timeList[0].start.format()}
-                  endDate={timeList[0].start.format()}></ItemSummary>
-              </Tab>
-              <Tab name="上月" class={s.tab}>
-                <ItemSummary startDate={timeList[1].start.format()}
-                  endDate={timeList[1].start.format()}></ItemSummary>
-              </Tab>
-              <Tab name="今年" class={s.tab}>
-                <ItemSummary startDate={timeList[1].start.format()}
-                  endDate={timeList[2].start.format()}></ItemSummary>
-              </Tab>
-              <Tab name="自定义时间" class={s.tab}>
-                <ItemSummary startDate={customTime.startDate}
-                  endDate={customTime.endDate}></ItemSummary>
-              </Tab>
-            </Tabs>
-          )
+            <>
+              <Tabs
+                class={s.items}
+                classPrefix={"custom_items"}
+                v-model:selected={selectTab.value}
+                onUpdate:selected={(item) => {
+                  if (item === "自定义时间") {
+                    refOverlayVisible.value = true;
+                  }
+                }}
+              >
+                <Tab name="本月" class={s.tab}>
+                  <ItemSummary
+                    startDate={timeList[0].start.format()}
+                    endDate={timeList[0].start.format()}
+                  ></ItemSummary>
+                </Tab>
+                <Tab name="上月" class={s.tab}>
+                  <ItemSummary
+                    startDate={timeList[1].start.format()}
+                    endDate={timeList[1].start.format()}
+                  ></ItemSummary>
+                </Tab>
+                <Tab name="今年" class={s.tab}>
+                  <ItemSummary
+                    startDate={timeList[1].start.format()}
+                    endDate={timeList[2].start.format()}
+                  ></ItemSummary>
+                </Tab>
+                <Tab name="自定义时间" class={s.tab}>
+                  <ItemSummary
+                    startDate={customTime.start}
+                    endDate={customTime.end}
+                  ></ItemSummary>
+                </Tab>
+              </Tabs>
+              <Overlay show={refOverlayVisible.value} class={s.overlay}>
+                <div class={s.overlay_inner}>
+                  <header>请选择时间</header>
+                  <main>
+                    <Form onSubmit={onSubmitCustomTime}>
+                      <FormItem
+                        label="开始时间"
+                        type="date"
+                        v-model={customTime.start}
+                      ></FormItem>
+                      <FormItem
+                        label="结束时间"
+                        type="date"
+                        v-model={customTime.end}
+                      ></FormItem>
+
+                      <FormItem>
+                        <div class={s.actions}>
+                          <button
+                            type="button"
+                            onClick={() => (refOverlayVisible.value = false)}
+                          >
+                            取消
+                          </button>
+                          <button type="submit">确认</button>
+                        </div>
+                      </FormItem>
+                    </Form>
+                  </main>
+                </div>
+              </Overlay>
+            </>
+          ),
         }}
       </MainLayout>
-    )
-  }
-})
+    );
+  },
+});
--- src/components/tag/tag-form.tsx
+++ src/components/tag/tag-form.tsx
@@ -2,31 +2,46 @@ import { defineComponent, reactive } from "vue";
 import { Button } from "../../shared/button";
 import { Form, FormItem } from "../../shared/form";
 import { Rules, validate } from "../../shared/validate";
-import s from './tag-create.module.scss';
+import s from "./tag-create.module.scss";
 
 export const TagForm = defineComponent({
   setup: () => {
     const formData = reactive({
-      name: '',
-      sign: ''
-    })
+      name: "",
+      sign: "",
+    });
     const errors = reactive<{ [k: string]: string[] }>({});
     const rules: Rules<typeof formData> = [
-      { key: 'name', type: 'required', message: '必填' },
-      { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
-      { key: 'sign', type: 'required', message: '必填' }
-    ]
+      { key: "name", type: "required", message: "必填" },
+      {
+        key: "name",
+        type: "pattern",
+        regex: /^.{1,4}$/,
+        message: "只能填 1 到 4 个字符",
+      },
+      { key: "sign", type: "required", message: "必填" },
+    ];
     const onFormSubmit = (e: Event) => {
       e.preventDefault();
-      Object.assign(errors, {name: undefined, sign: undefined});
+      Object.assign(errors, { name: undefined, sign: undefined });
       Object.assign(errors, validate(formData, rules));
       console.log(errors.value);
-    }
+    };
 
     return () => (
       <Form onSubmit={onFormSubmit}>
-        <FormItem label="标签名" type="text" error={errors['name']?.join(',')} v-model={formData.name}></FormItem>
-        <FormItem label="符号" type="emojiSelect" error={errors['sign']?.join(',')} v-model={formData.sign}></FormItem>
+        <FormItem
+          label="标签名"
+          type="text"
+          error={errors["name"]?.join(",")}
+          v-model={formData.name}
+        ></FormItem>
+        <FormItem
+          label="符号"
+          type="emojiSelect"
+          error={errors["sign"]?.join(",")}
+          v-model={formData.sign}
+        ></FormItem>
         <FormItem>
           <p class={s.tips}>记账时长按标签，即可进行编辑</p>
         </FormItem>
@@ -34,6 +49,6 @@ export const TagForm = defineComponent({
           <Button class={s.button}>确定</Button>
         </FormItem>
       </Form>
-    )
-  }
+    );
+  },
 });
